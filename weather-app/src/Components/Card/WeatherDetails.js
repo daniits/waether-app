ca003458051ce0a25558dab860d0648c64bbf151
Daniyal <danyal@antiquelogic.com>
2023-08-21 16:54:53 +0500

    remove-console

--- weather-app/src/Components/Card/WeatherDetails.js
+++ weather-app/src/Components/Card/WeatherDetails.js
@@ -2,7 +2,6 @@ import Image from 'next/image'
 import React from 'react'
 
 const WeatherDetails = ({ data }) => {
-  console.log(data)
   return (
     <div className="relative flex flex-col justify-between max-w-[500px] w-full h-[90vh] m-auto p-4 text-gray-300 z-10">
       {/* Top */}
